da8a35e7aeb50550afc125cd0bac805e3e1d5719
drillteamensmm <drillteamensmm@gmail.com>
2025-10-13 20:25:33 +0200

    Adding Dithering

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import Dither from './Dither.jsx'
+import './Dither.css'
+
+
+createRoot(document.getElementById('root')).render(
+<React.StrictMode>
+<Dither />
+</React.StrictMode>
+)
